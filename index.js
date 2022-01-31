754a9a98afef1d91a0aaaa67c27474e5a81086c9
MrKenobii <anldygc35@windowslive.com>
2022-01-31 18:54:26 +0300

    node callbacks and error handling

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const rect = require('./rectangle');
 const solveRect = (l,b) => {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
-    if(l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should greater than 0");
-    }else{
-        console.log("The are of the rectangle is " + rect.area(l,b) + " and the perimeter is " + rect.perimeter(l,b));
-    }
+    rect(l,b,(err, rectangle) => {
+        if(err) console.log("Error: " + err.message);
+        else{
+            console.log("The area of the rectangle of dimensions l = " + l + " and b = " + b + " is: " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = " + l + " and b = " + b + " is: " + rectangle.perimeter());
+        } 
+    });
+    console.log("This statement is after the call to rect()");
 }
 solveRect(2,4);
 solveRect(3,5);
